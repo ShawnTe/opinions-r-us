8d8d9d91958157cabcde221d5913b495ea8a441d
hypatiah <danielle.hassid@gmail.com>
2016-08-12 16:36:24 -0700

    Add search bar in layout to search questions and questions of users

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,21 @@
 get '/' do
+  @questions = Question.all
+  erb :index
+end
+
+get '/search' do
+  @questions = []
+  content_match = Question.where(["content LIKE ?", "%#{params[:query]}%"])
+  user_match = Question.where(["user LIKE ?", "%#{params[:query]}%"])
+  if content_match
+    content_match.each do |question|
+      @questions << question
+    end
+  end
+  if user_match
+    user_match.each do |question|
+      @questions << question
+    end
+  end
   erb :index
 end
