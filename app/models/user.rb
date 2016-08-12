485643a4a280ccd932c035642ac82381040dcc91
Eric Booker <ericbooker12@gmail.com>
2016-08-12 16:15:50 -0700

    Create associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  has_many :questions, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :answers, foreign_key: :author_id
+  has_many :votes
 
   has_secure_password
 
