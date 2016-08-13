34627f98df904b8141fd3a81dde52773f3ea080b
hypatiah <danielle.hassid@gmail.com>
2016-08-12 17:38:01 -0700

    Add search bar functionality to find questions of given user's name

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -6,7 +6,13 @@ end
 get '/search' do
   @questions = []
   content_match = Question.where(["content LIKE ?", "%#{params[:query]}%"])
-  # user_match = User.where(["name LIKE ?", "%#{params[:query]}%"])
+  # user_match = Question.where(["SELECT * FROM users WHERE name LIKE ?", "%#{params[:query]}%"])
+  user_match = User.where(["name LIKE ?", "%#{params[:query]}%"])
+  user_match.each do |user|
+    user.questions.each do |question|
+      @questions << question
+    end
+  end
   if content_match
     content_match.each do |question|
       @questions << question
